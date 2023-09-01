649854be0d0a2551323548b98dd2e6ef2b86f8d3
SubhashRayapureddi <90317420+SubhashRayapureddi@users.noreply.github.com>
2023-09-01 09:35:36 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import tensorflow as tf
 import pandas_datareader as data
 from datetime import datetime, timedelta, date
 import yfinance as yfin
